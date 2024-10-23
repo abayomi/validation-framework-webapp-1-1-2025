625cdd54d4a089d1e997c0342214b1a75c515583
Max Wang <mawang@elementcorp.com>
2024-10-23 16:27:52 -0400

    Formatted code

--- app/page.js
+++ app/page.js
@@ -2,16 +2,15 @@
 import styles from "./page.module.css";
 import About from  "./components/about/about";
 import Counter from "./components/counter/Counter";
-import Navbar from './components/navbar/Navbar';
+import Navbar from "./components/navbar/Navbar";
 import Profile from "./components/profile/Profile";
 import TabMenu from "./components/createobject/tabMenu";
 import ViewTabMenu from "./components/homepage/tabMenu";
 import { useState, useEffect } from "react";
-
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import store from './store/store';
-import './App.scss';
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store/store";
+import "./App.scss";
 
 import {
   ApolloClient,
@@ -49,40 +48,42 @@ export default function Home() {
   useEffect(() => {    
     setIsBrowser(typeof window !== "undefined");
   }, [isBrowser]);
+
+  const browserRouter = (
+    <BrowserRouter>
+        <Provider store={store} >
+          <div className="App">
+            <Navbar />
+            <div className="container mt-2" style={{ marginTop: 40 }}>
+              <Routes>
+                <Route path="/" element={ <ViewTabMenu /> } />
+                <Route path="about" element={ <About /> } />          
+                <Route path="counter" element={ <Counter /> } />
+                <Route path="profile/:name" element={ <Profile /> }/>              
+                <Route path="createmasterobject" element={ <TabMenu /> } />        
+                <Route path="updatemasterobject/object" element={ <TabMenu /> } /> 
+                <Route path="updatemasterobject/field" element={ <TabMenu /> } />  
+              </Routes>
+            </div>
+
+            <footer className="bg-white">
+              <div className="bg-light py-4">
+                <div className="container text-center">
+                  <p className="text-muted mb-0 py-2">Validation Framework</p>
+                </div>
+              </div>
+            </footer>
+          </div>
+        </Provider>
+    </BrowserRouter>
+  );
+
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-      <ApolloProvider client={client}>
-      {isBrowser? <BrowserRouter>
-        <Provider store={store} >
-          
-
-          <div className="App">
-          <Navbar />
-          <div className="container mt-2" style={{ marginTop: 40 }}>
-            <Routes>
-              <Route path="/" element={ <ViewTabMenu/> } />
-              <Route path="about" element={ <About/> } />          
-              <Route path="counter" element={ <Counter/> } />
-              <Route path="profile/:name" element={ <Profile/> }/>              
-              <Route path="createmasterobject" element={ <TabMenu/> } />        
-              <Route path="updatemasterobject/object" element={ <TabMenu/> } /> 
-              <Route path="updatemasterobject/field" element={ <TabMenu/> } />  
-            </Routes>
-          </div>
-
-          <footer className="bg-white">
-            <div className="bg-light py-4">
-              <div className="container text-center">
-                <p className="text-muted mb-0 py-2">Validation Framework</p>
-              </div>
-            </div>
-          </footer>
-        </div>
-
-        </Provider>
-      </BrowserRouter>: null}
-    </ApolloProvider>
+        <ApolloProvider client={client}>
+          {isBrowser ? browserRouter : null}
+        </ApolloProvider>
       </div>
     </main>
   );
