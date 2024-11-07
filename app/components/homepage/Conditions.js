8f274d8393258a10269532701a382960e2177437
Max Wang <mawang@elementcorp.com>
2024-11-07 10:19:24 -0500

    Hide the conditions for a rule that allows blank.

--- app/components/homepage/Conditions.js
+++ app/components/homepage/Conditions.js
@@ -2,19 +2,20 @@
 import withAuth from "../withAuth";
 
 const Conditions = ({ conditionData }) => {
-    return (
-        <>
-            {conditionData.map(item => (
-                <ul key={item.id}>
-                    <li>Id: <b>{item.id}</b></li>
-                    <li>Type: <b>{item.type}</b></li>
-                    <li>Value: <b>{item.value}</b></li>
-                    <li>Short Description: <b>{item.shortDescription}</b></li>
-                    <li>Long Description: <b>{item.longDescription}</b></li>
-                </ul>
-            ))}
-        </>
-    );
+    const isAllowBlankRule = (0 === conditionData.length) || !conditionData[0].id;
+    if (isAllowBlankRule) {
+        return <p>No conditions for a rule that allows blank.</p>;
+    }
+
+    return conditionData.map(item => (
+        <ul key={item.id}>
+            <li>Id: <b>{item.id}</b></li>
+            <li>Type: <b>{item.type}</b></li>
+            <li>Value: <b>{item.value}</b></li>
+            <li>Short Description: <b>{item.shortDescription}</b></li>
+            <li>Long Description: <b>{item.longDescription}</b></li>
+        </ul>
+    ));
 }
 
 export default withAuth(Conditions);
