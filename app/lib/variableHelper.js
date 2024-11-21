8152cc9cf11c03bcbc2cb2aa6c64f949954a02f8
Max Wang <mawang@elementcorp.com>
2024-11-21 12:14:55 -0500

    Comment

--- app/lib/variableHelper.js
+++ app/lib/variableHelper.js
@@ -1,9 +1,17 @@
 const variableHelper = {};
 
+/**
+ * const arr = [];
+ * console.log(variableHelper.isArray(arr)); // Output is true
+ */
 variableHelper.isArray = function(arr) {
     return arr instanceof Array;
 };
 
+/**
+ * const obj = {};
+ * console.log(variableHelper.isEmptyObject(obj)); // Output is true
+ */
 variableHelper.isEmptyObject = function(obj) {
     return !(obj instanceof Object) || 0 === Object.keys(obj).length;
 }
