bc44c95056ecfb566d16ced0ae20093124cd02c8
Lei Zhang <lzhang@elementcorp.com>
2024-11-29 16:11:56 -0500

    add key into TabMenu

--- app/components/homepage/homepageTabMenu.js
+++ app/components/homepage/homepageTabMenu.js
@@ -1,13 +1,20 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Tab from 'react-bootstrap/Tab';
 import Tabs from 'react-bootstrap/Tabs';
 import ViewObjectMaster from './viewObjectMaster';
 import ViewFieldMaster from './viewFieldMaster'
 import { useLocation } from "react-router-dom";
 
-const HomepageTabMenu = () => {  
+const HomepageTabMenu = ({ tabKey = 'viewObjectMaster' }) => {  
+  console.log(tabKey);
   const location = useLocation();
-  const [key, setKey] = useState('ViewObjectMaster'); // Set up the default tab. The value is the eventKey of a <Tab>.
+  const [key, setKey] = useState(tabKey); // Set up the default tab. The value is the eventKey of a <Tab>.
+
+  useEffect(() => {
+    if (location.state?.tabKey) {
+      setKey(location.state.tabKey);
+    }
+  }, [location]);
 
   return (
     <Tabs
@@ -16,7 +23,7 @@ const HomepageTabMenu = () => {
       onSelect={(k) => setKey(k)}
       className="mb-3"
     >
-      <Tab eventKey="ViewObjectMaster" title="Object Master">
+      <Tab eventKey="viewObjectMaster" title="Object Master">
         <ViewObjectMaster location={location} />
       </Tab>
       <Tab eventKey="viewFieldMaster" title="Field Master">        
