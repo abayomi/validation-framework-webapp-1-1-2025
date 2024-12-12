8b8680cdaff9a5f769d58c64968e6a327f63583d
Max Wang <mawang@elementcorp.com>
2024-12-12 16:54:12 -0500

    Optimize import

--- app/components/createobject/createObjectFields.js
+++ app/components/createobject/createObjectFields.js
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
