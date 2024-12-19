97b1f4f482cc3c590d7965947f2b4919c4844fd1
Max Wang <mawang@elementcorp.com>
2024-12-19 15:19:31 -0500

    Comment

--- test/createObjectMaster.test.js
+++ test/createObjectMaster.test.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import '@testing-library/jest-dom';
-import { fireEvent, render, screen, waitFor } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import CreateObjectMaster from '../app/components/createobject/createObjectMaster';
 import {ApolloClient, InMemoryCache, useMutation} from "@apollo/client";
 import { useQuery } from '@apollo/client';
+
+/**
+ * You might wonder why this unit test is so simple. There are two reasons:
+ *
+ *     1. In the CreateObjectMaster component, the most important subcomponent is CreateObjectFields, which has its own unit test file, createObjectFields.test.js.
+ *     2. For the creation and editing functions of Object Master, the critical logic resides in createObjectMasterLogic.js, which also has its own unit test file.
+ *
+ * Based on the above reasons, there is little left to test in this component, so this unit test mainly mocks necessary third-party libraries. If this test is later
+ * deemed insufficient, the unit test coverage can be improved by exporting the logic unrelated to UI display as independent functions and supplementing their tests
+ * in this unit test file. However, this might not be very meaningful.
+ */
 
 jest.mock('@apollo/client', () => {
     const actualApolloClient = jest.requireActual('@apollo/client');
